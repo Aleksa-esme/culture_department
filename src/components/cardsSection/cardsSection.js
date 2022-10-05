5e116bb5f1e9e8c264a6df082871a1a12918553d
Aleksa-esme <esme1990@mail.ru>
2022-10-05 17:15:03 +0300

    feat/mobile layout

--- src/components/cardsSection/cardsSection.js
+++ src/components/cardsSection/cardsSection.js
@@ -1,3 +1,16 @@
+import img1 from '../../assets/images/1.webp';
+import img2 from '../../assets/images/2.webp';
+import img3 from '../../assets/images/3.webp';
+import img4 from '../../assets/images/4.webp';
+import img5 from '../../assets/images/5.webp';
+import img6 from '../../assets/images/6.webp';
+import img7 from '../../assets/images/7.webp';
+import img8 from '../../assets/images/8.webp';
+import img9 from '../../assets/images/9.webp';
+import img10 from '../../assets/images/10.webp';
+import img11 from '../../assets/images/11.webp';
+import img12 from '../../assets/images/12.webp';
+
 import { Card } from './card/card';
 
 import styles from './cardsSection.module.css';
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,10 @@
+import classNames from 'classnames';
 import { navLinks } from './data';
 import styles from './header.module.css';
 
 export const Header = () => {
+  const cnButton = classNames("dropdown", styles.menuButton);
+  const cnDropdown = classNames("dropdown-menu", styles.dropdownMenu);
   return (
     <header className={styles.header}>
       <div className={styles.headerContent}>
@@ -12,6 +15,20 @@ export const Header = () => {
             </a>
           ))}
         </nav>
+        <div className={cnButton}>
+          <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Меню
+          </button>
+          <ul className={cnDropdown}>
+            {navLinks.map(({ title, link }) => (
+              <li key={title} className={styles.menuElement}>
+                <a href={link} className={styles.navLink}>
+                  {title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </header>
   );
